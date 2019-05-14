9bd9424814858e8a25c8ab217412cc791a2b8ab3
DaShinja <byron@dashinja.com>
2019-05-14 02:35:25 -0400

    add few more tests to factorial_test.js

--- test/factorial_test.js
+++ test/factorial_test.js
@@ -5,9 +5,18 @@ describe('Completes factorial product of given number', function() {
   it('It should return 4 * 3 * 2 * 1', function() {
     expect(factorial(4)).to.equal(24)
   })
-})
-
-describe('Error Response for non-number arguments', () => {
+  it('If n = 0, should return 1', function() {
+    expect(factorial(0)).to.equal(1)
+  })
+  it('If n = 5, should return 120', function() {
+    expect(factorial(5)).to.equal(120)
+  })
+  it('If n = 10, should return 3628800', function() {
+    expect(factorial(10)).to.equal(3628800)
+  })
+  it('If n = 20, should return 2432902008176640000', function() {
+    expect(factorial(20)).to.equal(2432902008176640000)
+  })
   it("Returns NaN if function arguments aren't numbers...", () => {
     expect(factorial(4)).to.be.a('number')
   })
